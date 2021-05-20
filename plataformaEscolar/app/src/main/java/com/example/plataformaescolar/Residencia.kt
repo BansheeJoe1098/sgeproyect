573f0a0fc59aad15b1fddcf5022e1ae74704bcb2
Joe Miguel <miguerdz1023@gmail.com>
2021-05-19 19:18:37 -0500

    Residencias
    botones y regsitros

---
+++ plataformaEscolar/app/src/main/java/com/example/plataformaescolar/Residencia.kt
@@ -0,0 +1,42 @@
+package com.example.plataformaescolar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.json.JSONObject
+import com.example.plataformaescolar.databinding.ActivityResidenciaBinding
+
+class Residencia : AppCompatActivity() {
+    private lateinit var binding:ActivityResidenciaBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResidenciaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var usuario = intent.getStringExtra("usuario")
+        val jsonUsuario = JSONObject(usuario)
+        val jsonUsuarioEdit = jsonUsuario
+        println(jsonUsuario)
+
+        binding.numeroControlR.setText(jsonUsuario.getString("noControl"))
+        binding.nombreR.setText(jsonUsuario.getString("nombre"))
+        binding.carreraR.setText(jsonUsuario.getString("carrera"))
+        var semestre = binding.semestreR.setText(jsonUsuario.getString("semestre"))
+        binding.contrasenaR.setText(jsonUsuario.getString("contrasena"))
+
+        binding.btnRegistro.setOnClickListener {
+
+        }
+
+        binding.btnRegresar.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("usuario", jsonUsuarioEdit.toString())
+            startActivity(intent)
+            finish()
+        }
+
+
+
+    }
+}
