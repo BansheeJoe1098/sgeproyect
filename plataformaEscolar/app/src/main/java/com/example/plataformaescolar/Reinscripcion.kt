4b336d3ff97092c43843d8d5bdaa253ff7af9484
Joe Miguel <miguerdz1023@gmail.com>
2021-05-19 22:06:52 -0500

    hola

--- plataformaEscolar/app/src/main/java/com/example/plataformaescolar/Reinscripcion.kt
+++ plataformaEscolar/app/src/main/java/com/example/plataformaescolar/Reinscripcion.kt
@@ -7,5 +7,6 @@ class Reinscripcion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reinscripcion)
+
     }
 }
